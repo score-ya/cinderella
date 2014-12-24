04590761dd70bcae3e223b34f354957569614116
Alexander Miehe <thelex@beamscore.com>
2014-12-24 15:20:42 +0100

    * init cinderella

---
+++ src/CoreBundle/DependencyInjection/ServiceLoadExtensionTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ScoreYa\Cinderella\Bundle\CoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * @author Alexander Miehe <thelex@beamscore.com>
+ *
+ * @codeCoverageIgnore
+ */
+trait ServiceLoadExtensionTrait
+{
+    /**
+     * @param ContainerBuilder $container
+     * @param string           $path
+     */
+    protected function loadServices(ContainerBuilder $container, $path)
+    {
+        $servicesPath = $path . '/../Resources/config/services';
+        $loader = new XmlFileLoader($container, new FileLocator($servicesPath));
+
+        $finder = new Finder();
+
+        /** @var $file SplFileInfo */
+        foreach ($finder->in($servicesPath)->files()->name('*.xml') as $file) {
+            $loader->load($file->getFilename());
+        }
+    }
+}
---
+++ src/TemplateApiBundle/ScoreYaCinderellaTemplateApiBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ScoreYa\Cinderella\Bundle\TemplateApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Alexander Miehe <thelex@beamscore.com>
+ *
+ * @codeCoverageIgnore
+ */
+class ScoreYaCinderellaTemplateApiBundle extends Bundle
+{
+
+}
